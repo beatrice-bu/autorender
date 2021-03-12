0d162bc3f40eca9be55463d7cc3b7b551a52190f
Beatrice-bu <beatrice.au.bu@gmail.com>
2021-03-12 18:35:37 -0500

    almost ready

--- render_script.py
+++ render_script.py
@@ -1,22 +1,71 @@
-from random import uniform
-from mathutils import Vector
+import math
 import bpy
+import mathutils
+import os
+import glob
+import random
+from mathutils import Vector
+
+path_to_hdrs = 'D:\\Programming\\github\\autorender\\hdrs\\'
+path_to_output = 'D:/Programming/output'
+img_name = 'chair'
 
 
+def initializeBG():    
+    
+    C = bpy.context
+    world = C.scene.world
+    world.use_nodes = True
+
+    global env_node
+    env_node = C.scene.world.node_tree.nodes.new("ShaderNodeTexEnvironment")
+    
+    node_tree = C.scene.world.node_tree 
+    node_tree.links.new(env_node.outputs['Color'], node_tree.nodes['Background'].inputs['Color'])
+    
+    bpy.context.scene.render.film_transparent = False
+    
+    
+    for file in glob.glob(path_to_hdrs + "*.hdr"):
+        
+        print(f'Loading file : {file}')
+        bpy.data.images.load(file)
+        
+     
+    
+initializeBG()
+img_random = random.randint(0, len(bpy.data.images))
+print(img_random)
+env_node.image = bpy.data.images[img_random]
+print(env_node.image)
+    
 
 
+def autoRenderCycle(cycles: int)
 
-bpy.ops.object.select_all(action='SELECT')
-bpy.ops.object.delete(use_global=False)
 
-bpy.ops.object.camera_add(enter_editmode=False, align='VIEW', location=(0, 0, 0), rotation=(1.24267, -7.30479e-07, -0.418879), scale=(1, 1, 1))
-bpy.ops.wm.collada_import(filepath="D:\\Programming\\github\\image-gen\\models\\chair.dae")
-bpy.ops.object.select_all(action='DESELECT')
+for c in range(cycles):
+    x = uniform(-5,5)
+    y = uniform(-5,5)
+    if (x >= -1.5 and x <= 1.5) and (y >= -1.5 and y <= 1.5):
+        z = uniform(3,5)
+    else:
+        z = uniform(0,5)
+    cam.location = Vector((x, y, z))
+    
+    
+    
+    bpy.context.scene.render.engine = 'CYCLES'
+    bpy.context.scene.render.image_settings.color_mode ='RGBA'
+    bpy.context.scene.render.image_settings.file_format='PNG' 
+    bpy.context.scene.render.image_settings.compression = 90
+
+    bpy.context.scene.render.filepath = os.path.join(path_to_output, f'{img_name}-{str(cycle)}.png')
+    bpy.ops.render.render(write_still=True)
 
 
 chair = bpy.data.objects['Chair']
-chair.location = Vector((0,0,0.5))
-
+chair.location = Vector((0,0,0.5)
 
 cam = bpy.data.objects['Camera']
 
@@ -32,20 +81,46 @@ bpy.context.object.constraints["Track To"].track_axis = 'TRACK_NEGATIVE_Z'
 
 
 
-cam = bpy.data.objects['Camera']
-def randX():
-    x = uniform(-5,5)
-    y = uniform(-5,5)
-    if (x >= -1.5 and x <= 1.5) and (y >= -1.5 and y <= 1.5):
-        z = uniform(3,5)
-    else:
-        z = uniform(0,5)
-    cam.location = Vector((x, y, z))
+class autorenderPanel(bpy.types.Panel):
+    bl_label = "Auto Render"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_context = "objectmode"
+    bl_category = "Autorender"
     
+    
+    def draw(self, context):
+        row = self.layout.row()
+        row.label(text='Autorendering script')
+        column = self.layout.column(align=True)
+        column.operator("render.initialize", text="Initialize Environment"
+        column.operator("render.autorender", text="Begin AutoRendering")
+        
+class autorender(bpy.types.Operator):
+    bl_idname = "render.autorender"
+    bl_label = "Initialize"
+    
+    def execute(self, context):
+        print ("Initializing")
+        initializeBG()
+        return {'FINISHED'}     
+        
+        
+        
+class autorender(bpy.types.Operator):
+    bl_idname = "render.autorender"
+    bl_label = "Initialize"
+    
+    def execute(self, context):
+        print ("Initializing")
+        initializeBG()
+        return {'FINISHED'}
 
+cl
+    
+  
+    
+bpy.utils.register_class(autorenderPanel)
+bpy.utils.register_class(autorender)
 
-randX()
-
-
-
-
+'''
